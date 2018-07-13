f3222c769f3d8f043682aeb171b18c36ad4ea2cf
shariq786 <40831477+shariq786@users.noreply.github.com>
2018-07-13 12:10:05 +0500

    info

--- application/views/admin/product/view.php
+++ application/views/admin/product/view.php
@@ -9,7 +9,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Products</h1>
+            <h1 class="m-0 text-dark">Products Information</h1>
       
           </div><!-- /.col -->
           <div class="col-sm-6">
